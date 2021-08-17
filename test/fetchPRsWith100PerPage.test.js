0b52a1c6afccb9163e7ebb602f9a231088a75024
mjperales <m.j.perales@tcu.edu>
2021-08-17 11:58:48 -0500

    fix: stub http requests

--- test/fetchPRsWith100PerPage.test.js
+++ test/fetchPRsWith100PerPage.test.js
@@ -1,17 +1,32 @@
-const expect = require('chai').expect;
-const fetchPRsWith100PerPage = require('../bin/fetchPRsWith100PerPage');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+const response = require('./fixtures/github-data.json');
 
 describe('fetchPRsWith100PerPage() tests', function () {
-    it('returns page not found if wrong/bad repository name', async function () {
-        const rsp = await fetchPRsWith100PerPage('ramda', 'mayra');
-        expect(rsp).equal('Not Found');
-    });
-    it('returns page not found if wrong/bad organization name', async function () {
-        const rsp = await fetchPRsWith100PerPage('mayra', 'ramda');
-        expect(rsp).equal('Not Found');
+    it('returns page not found if wrong/bad repository name or orgName', async function () {
+        const fetchPRsWith100PerPage = sinon.stub().withArgs('ramda', 'mayra');
+        fetchPRsWith100PerPage.returns(
+            JSON.parse(response.all.error.res.status)
+        );
+
+        expect(fetchPRsWith100PerPage('ramda', 'mayra')).equal(404);
     });
     it('returns object with successful response', async function () {
-        const rsp = await fetchPRsWith100PerPage('ramda', 'ramda-fantasy');
-        expect(rsp).to.be.a('object');
+        const fetchPRsWith100PerPage = sinon
+            .stub()
+            .withArgs('ramda', 'ramda-fantasy');
+        fetchPRsWith100PerPage.returns(response.all.success.res);
+        expect(fetchPRsWith100PerPage('ramda', 'ramda-fantasy')).to.be.a(
+            'object'
+        );
+    });
+    it('returns an array with 3 items', async function () {
+        const fetchPRsWith100PerPage = sinon
+            .stub()
+            .withArgs('ramda', 'ramda-fantasy');
+        fetchPRsWith100PerPage.returns(response.all.success.res.data);
+        response.all.success.res.data.length.should.equal(3);
     });
 });
